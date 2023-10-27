a333af8380b682e332941417f70b7456ea39d7fc
XiweiShe5 <108431428+XiweiShe5@users.noreply.github.com>
2023-10-26 18:25:00 -0700

    Update getROICurve.m

--- toolbox/stability/getROICurve.m
+++ toolbox/stability/getROICurve.m
@@ -1,8 +1,8 @@
-function [EEG, ROI_x, ROI_y, CI_x, CI_y] = getROICurve(EEG, elecs, tepName)
+function [EEG, ROI_x, ROI_y, CI_x, CI_y] = getROICurve(EEG, elecs, tepName, selectedTimeSpan)
 
     % TESA - ROI
     EEG = pop_tesa_tepextract(EEG, 'ROI', 'elecs', elecs, 'tepName', tepName);
-    pop_tesa_plot(EEG, 'tepType', 'ROI', 'tepName', tepName, 'CI', 'on');
+    pop_tesa_plot(EEG, 'tepType', 'ROI', 'tepName', tepName, 'CI', 'on','xlim',[selectedTimeSpan(1), selectedTimeSpan(2)-1]);
     fig = gcf; dataObjs = findobj(fig,'-property','YData');
     ROI_x = dataObjs(3).XData;
     ROI_y = dataObjs(3).YData;

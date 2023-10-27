1ab403d0145972594bb6b54b046f88dd792e7cdf
XiweiShe5 <108431428+XiweiShe5@users.noreply.github.com>
2023-10-26 18:21:23 -0700

    Update TEPStabilityAnalysis_v2.m

--- TEPStabilityAnalysis_v2.m
+++ TEPStabilityAnalysis_v2.m
@@ -7,109 +7,127 @@ addpath('toolbox/eeglab2022.0'); % Dowload the eeglab toolbox and add it to this
 eeglab
 
 %% Load example dataset
-load('exampleData.mat');
 
 numChan = size(EEG.data, 1);
 numEpoch = size(EEG.data, 3);
 
-numTrialIncluded = 1:1:100;
+epochSeg = 10:5:100;
 
-visRange_x = [-101, 501];
-visRange_y = [-40, 40];
+visRange_x = [-100, 501];
+visRange_y = [-20, 20];
 
-dataRange = find(EEG.times==visRange_x(1)):find(EEG.times==visRange_x(2));
-numPoint = size(dataRange, 2)-1;
-
-%% Visualize original EEG set
-% pop_eegplot(EEG, 1, 1, 1);
-
-%% Calculate TEP ROI
 ROI = struct;
-ROI.LeftMotor = {'C3'}; % This dataset was left hemisphere stimulated
 
-ROI_x = zeros(length(fieldnames(ROI)), length(numTrialIncluded), numPoint);
-ROI_y = zeros(length(fieldnames(ROI)), length(numTrialIncluded), numPoint);
-ci_x = zeros(length(fieldnames(ROI)), length(numTrialIncluded), numPoint*2);
-ci_y = zeros(length(fieldnames(ROI)), length(numTrialIncluded), numPoint*2);
+% This example data was left-hemisphere stimulated
+ROI.Local = {'C3', 'C5', 'C1', 'FC3', 'CP3'};
 
-EEGs = cell(length(numTrialIncluded), 1);
-datasetLabels = cell(length(numTrialIncluded), 1);
-legendText = cell(length(numTrialIncluded), 1);
+ROI_x = zeros(length(fieldnames(ROI)), length(epochSeg), visRange_x(2) - visRange_x(1));
+ROI_y = zeros(length(fieldnames(ROI)), length(epochSeg), visRange_x(2) - visRange_x(1));
+ci_x = zeros(length(fieldnames(ROI)), length(epochSeg), (visRange_x(2) - visRange_x(1))*2);
+ci_y = zeros(length(fieldnames(ROI)), length(epochSeg), (visRange_x(2) - visRange_x(1))*2);
 
-% Sub-set of data based on number of epochs/trials
-for e = 1:length(numTrialIncluded)
-    
-    EEGs{e} = pop_select(EEG, 'trial', 1:numTrialIncluded(e), 'point', dataRange);
-    datasetLabels{e} = [mat2str(numTrialIncluded(e)), ' epochs'];
-    legendText{e} = [mat2str(numTrialIncluded(e)), ' epochs']; 
-    
-    % TESA - ROI
-    [EEGs{e}, ROI_x(1, e, :), ROI_y(1, e, :), ci_x(1, e, :), ci_y(1, e, :)] = ...
-    getROICurve(EEGs{e}, ROI.LeftMotor, 'LeftMotor');
-        
+for e = 1:length(epochSeg)
+    currentEpoch = epochSeg(e);
+
+    load(['ExampleData\dataPreprocessed_', mat2str(epochSeg(e)),'-epoch.mat'])
+
+    dataRange = find(EEG.times==visRange_x(1)):find(EEG.times==visRange_x(2));
+    numPoint = size(dataRange, 2)-1;
+    EEG = pop_select(EEG, 'point', dataRange);
+
+    % TESA - Local TEP
+    [~, ROI_x(1, e, :), ROI_y(1, e, :), ci_x(1, e, :), ci_y(1, e, :)] = ...
+        getROICurve(EEG, ROI.Local, 'LocalTEP', visRange_x);
+
 end
 
 %% Find minimal epoch of each ROI to achieve stability
-CCThreshold = 0.95;
+CCCThreshold = 0.8;
+MNP_early_index = zeros(length(fieldnames(ROI)), 1); % [15 80]
+MNP_late_index = zeros(length(fieldnames(ROI)), 1); % [80 350]
 
-minNumEpoch_entire = zeros(length(fieldnames(ROI)), 1); % [15 500]
+CCC_early = zeros(length(fieldnames(ROI)), length(epochSeg));
+CCC_late = zeros(length(fieldnames(ROI)), length(epochSeg));
+
 for r = 1:length(fieldnames(ROI))
 
     vis_x = squeeze(ROI_x(r, 1, :));
-    
-    tempTrigger = 0; triggerFlag = 0; tempThresh = 30;
 
-    % All stage - time range 15~500 ms
-    timeRange_all = find(vis_x==15):find(vis_x==500);
-    TEP_final_all = squeeze(ROI_y(r, end, timeRange_all));
-    for e = 1:length(numTrialIncluded)
-        tempTEP = squeeze(ROI_y(r, e, timeRange_all))';
+    % Early stage - time range 15~80 ms
+    timeRange_early = find(vis_x==15):find(vis_x==80);
+    TEP_final_early = squeeze(ROI_y(r, end, timeRange_early));
+    for e = 1:length(epochSeg)
+        tempTEP = squeeze(ROI_y(r, e, timeRange_early))';
         
-        tempCCC = f_CCC([tempTEP', TEP_final_all], CCThreshold);
+        tempCCC = f_CCC([tempTEP', TEP_final_early], CCCThreshold);
         tempCCC = tempCCC{1}.est;
+
+        CCC_early(r, e) = tempCCC;
+    end
+
+    % Later stage - time range 80~350 ms
+    timeRange_late = find(vis_x==80):find(vis_x==350);
+    TEP_final_late = squeeze(ROI_y(r, end, timeRange_late));
+    for e = 1:length(epochSeg)
+        tempTEP = squeeze(ROI_y(r, e, timeRange_late))';
         
-        if tempCCC > CCThreshold 
-            if triggerFlag == 1
-                tempTrigger = tempTrigger + 1;
-            end
-            triggerFlag = 1;
-        else
-            triggerFlag = 0;
-            tempTrigger = 0;
-        end
+        tempCCC = f_CCC([tempTEP', TEP_final_late], CCCThreshold);
+        tempCCC = tempCCC{1}.est;
 
-        if tempTrigger > tempThresh
-            minNumEpoch_entire(r) = numTrialIncluded(e) - tempTrigger + 1;
-            tempTrigger = 0; triggerFlag = 0;
-            break
-        end
-
-        if e == length(numTrialIncluded)
-            minNumEpoch_entire(r) = numTrialIncluded(e) - tempTrigger + 1;
-            tempTrigger = 0; triggerFlag = 0;
-        end
+        CCC_late(r, e) = tempCCC;
     end
 
 end
 
+% Now find the MNP based on CCC
+for c = 1:size(CCC_early, 2)
+    MNP_early_index(1) = -1;
+    if CCC_early(1, c) > CCCThreshold
+        if all(CCC_early(1, c+1:end) > CCCThreshold)
+            MNP_early_index(1) = c;
+            break;
+        end
+    end
+end
+
+for c = 1:size(CCC_late, 2)
+    MNP_late_index(1) = -1;
+    if CCC_late(1, c) > CCCThreshold
+        if all(CCC_late(1, c+1:end) > CCCThreshold)
+            MNP_late_index(1) = c;
+            break;
+        end
+    end
+end
+
+% Consider the step size
+MNP_early = epochSeg(MNP_early_index);
+MNP_late = epochSeg(MNP_late_index);
+
+%% Display results
+disp('==== Local TEP ===')
+LMFAResult = [MNP_early(1), MNP_late(1)];
+disp(LMFAResult);
+
 %% Plot TEPs
 fields = fieldnames(ROI);
-myColors = parula(length(EEGs));
+myColors = parula(length(epochSeg));
 
 for i = 1:length(fields)
     fig = figure('position', [50, 50, 1300, 900]);
-    plot([0 0], visRange_y, '-k', 'LineWidth', 1.5); hold on; % TMS on
+    plot([0 0], visRange_y, '-k', 'LineWidth', 2); hold on; % TMS on
+    plot([15 15], visRange_y, '--k', 'LineWidth', 1); hold on; % TMS on
+    plot([80 80], visRange_y, '--k', 'LineWidth', 1); hold on; % TMS on
     legendText2 = cell(50);
     eIndex = 1;
-    for e = 1:10:length(numTrialIncluded)
+    for e = 1:length(epochSeg)
         vis_x = squeeze(ROI_x(i, e, :));
         vis_y = squeeze(ROI_y(i, e, :));
         plot(vis_x, vis_y, 'color', myColors(e, :), 'LineWidth', 2); hold on
-        legendText2{eIndex} = [mat2str(e+9), ' epochs'];
+        legendText2{eIndex} = [mat2str(epochSeg(e)), ' epochs'];
         eIndex = eIndex + 1;
     end
-    xlim(visRange_x); ylim(visRange_y)
-    xlabel('Latency (ms)'); ylabel('TEPs');
+    xlim([0 350]); ylim(visRange_y)
+    xlabel('Time (ms)'); ylabel('TEPs'); title(['Early: ', mat2str(MNP_early(1)), '  Late: ', mat2str(MNP_late(1))])
     set(gca, 'LineWidth', 2, 'Box', 'off', 'FontSize', 24)
-    legend([{''}; legendText2(1:eIndex-1)'], 'FontSize', 8)
 end
